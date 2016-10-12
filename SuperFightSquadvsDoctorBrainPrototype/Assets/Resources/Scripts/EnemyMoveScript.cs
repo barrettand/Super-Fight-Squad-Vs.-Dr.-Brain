7c0ee05771b0d3ca92a3dd1897764cbd29963d4e
barrettand <barrett.and@husky.neu.edu>
2016-10-12 14:46:06 -0400

    Unity Project Added
    Added the Unity Project

---
+++ SuperFightSquadvsDoctorBrainPrototype/Assets/Resources/Scripts/EnemyMoveScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveScript : MonoBehaviour {
+    public GameObject player, bullet;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < player.transform.position.x - 2)
+        {
+            transform.Translate(0.05f, 0, 0);
+        }
+        if (transform.position.x > player.transform.position.x + 2)
+        {
+            transform.Translate(-0.05f, 0, 0);
+        }
+        if (transform.position.y < player.transform.position.y)
+        {
+            transform.Translate(0, 0.05f, 0);
+        }
+        if (transform.position.y > player.transform.position.y)
+        {
+            transform.Translate(0, -0.05f, 0);
+        }
+        if (transform.position.y == player.transform.position.y && (transform.position.x >= player.transform.position.x - 2) || (transform.position.x <= player.transform.position.x + 2)) {
+            if (bullet.transform.position.x <= transform.position.x + 2)
+            {
+                bullet.transform.Translate(0.1f, 0, 0);
+            }
+        }
+    }
+}
---
+++ SuperFightSquadvsDoctorBrainPrototype/Assets/Resources/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+    public GameObject fist;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8)
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8)
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 0)
+        {
+            transform.Translate(0, 0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -4)
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.Space) && fist.transform.position.x < transform.position.x + 2)
+        {
+            fist.transform.Translate(0.5f, 0, 0);
+        }
+        else
+        {
+            fist.transform.position = transform.position;
+        }
+    }
+}
