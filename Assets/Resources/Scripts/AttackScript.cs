5cbe698bad2e040fb1b5ca13e28668652f372ff4
Ian Swain <conanreh@gmail.com>
2016-10-14 19:50:11 -0400

    Restructured Git + Player Attacks

---
+++ Assets/Resources/Scripts/AttackScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackScript : MonoBehaviour {
+
+    private SpriteRenderer sr;
+
+    void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (sr.isVisible)
+        {
+            transform.Translate(0.5f, 0, 0);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+}
---
+++ Assets/Resources/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+    public GameObject bullet;
+    public float shootDelay;
+    private float shootTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        shootTimer += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8)
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8)
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 0)
+        {
+            transform.Translate(0, 0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -4)
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.Space) && shootTimer >= shootDelay)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            shootTimer = 0;
+        }
+    }
+}
