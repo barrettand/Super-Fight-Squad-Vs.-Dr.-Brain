4d6cb01f2cea2b443d73e37fd919b5d1bffff457
barrettand <barrett.and@husky.neu.edu>
2016-10-17 15:48:52 -0400

    Stuff and things
    Added sprites and code

---
+++ Assets/Resources/Scripts/CameraMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    GameObject player;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        cam = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        print(player.transform.localPosition.x > 0);
+        if (player.transform.localPosition.x > 0 && cam.transform.position.x <= 58.1) {
+            player.transform.localPosition = new Vector3(0, player.transform.localPosition.y, player.transform.localPosition.z);
+            cam.transform.Translate(0.1f, 0, 0);
+        }
+        if (player.transform.localPosition.x < 0 && cam.transform.position.x >= 0)
+        {
+            player.transform.localPosition = new Vector3(0, player.transform.localPosition.y, player.transform.localPosition.z);
+            cam.transform.Translate(-0.1f, 0, 0);
+        }
+    }
+}
--- Assets/Resources/Scripts/PlayerMoveScript.cs
+++ Assets/Resources/Scripts/PlayerMoveScript.cs
@@ -17,7 +17,7 @@ public class PlayerMoveScript : MonoBehaviour {
     {
         shootTimer += Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8)
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > 0)
         {
             if (transform.localScale.x > 0) {
                 transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
@@ -25,7 +25,7 @@ public class PlayerMoveScript : MonoBehaviour {
             transform.Translate(0.1f, 0, 0);
             bullet.GetComponent<AttackScript>().left = true;
         }
-        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8)
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 58.1)
         {
             if (transform.localScale.x < 0)
             {
